8bf46a06dea0398221e10cfdf8edeb68ce4243c7
Affandi21 <affandishafwansoleh@gmail.com>
2025-11-23 23:51:16 +0700

    final

--- src/index.js
+++ src/index.js
@@ -11,9 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true
-}));
+    origin: [
+      "http://localhost:5173",
+      "https://tugas-tracker-fe-r7mi.vercel.app/"
+    ]
+  }));
 app.use(express.json());
 
 // Routes
